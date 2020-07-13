5937e8793a0fd5ec5441f7b2bc117cf023bbc6ae
a982338665 <982338665@qq.com>
2020-07-13 10:29:14 +0800

    8-支付模块构建1

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-17 21:13
+ */
+@SpringBootApplication
+//@EnableEurekaClient
+//@EnableDiscoveryClient
+public class PaymentMain8001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
