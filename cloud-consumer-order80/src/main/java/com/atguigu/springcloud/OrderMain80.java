66e49a361a38f30b2985be6f7fe7465c9562d3da
a982338665 <982338665@qq.com>
2020-07-22 11:29:49 +0800

    18-80入驻eureka

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -2,15 +2,16 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @auther zzyy
  * @create 2020-02-18 17:20
  */
 @SpringBootApplication
-public class OrderMain80
-{
+@EnableEurekaClient
+public class OrderMain80 {
     public static void main(String[] args) {
-            SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(OrderMain80.class, args);
     }
 }
