49afeaf754458d9fcd9c2dd2964a2ea595665841
a982338665 <982338665@qq.com>
2020-07-22 14:46:12 +0800

    22-支付微服务集群配置

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig
 {
     @Bean
+    @LoadBalanced//负载均衡，默认轮询，为了能够识别    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
     public RestTemplate getRestTemplate()
     {
         return new RestTemplate();
